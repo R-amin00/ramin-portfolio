bfc3ae0180248f7a9cccd00cd6e063d9647d0091
Ruhul <ruhulamin18753@gmail.com>
2023-07-10 01:35:55 +0600

    initialized

---
+++ src/Router/Routes/Routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../pages/Home/Home";
+import About from "../../pages/About/About";
+import Contact from "../../pages/Contact/Contact";
+import Projects from "../../pages/Projects/Projects";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/projects",
+        element: <Projects></Projects>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+    ],
+  },
+]);
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer footer-center p-8 bg-base-200 text-base-content rounded">
+        <div className="grid grid-flow-col gap-4">
+          <Link to="/projects" className="link link-hover uppercase">
+            Projects
+          </Link>
+          <Link to="/about" className="link link-hover uppercase">
+            About
+          </Link>
+          <Link to="/contact" className="link link-hover uppercase">
+            Contact
+          </Link>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a href="https://www.facebook.com/r.amin00000/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="23"
+                height="23"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+              </svg>
+            </a>
+            <a href="https://twitter.com/R_amin00">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="23"
+                height="23"
+                viewBox="0 0 24 24"
+                className="fill-current">
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+              </svg>
+            </a>
+            <a href="https://www.linkedin.com/in/ramin00/?originalSubdomain=bd">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                viewBox="0 0 1024 1024"
+                height="23px"
+                width="23px"
+                xmlns="http://www.w3.org/2000/svg">
+                <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1 1 68.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
